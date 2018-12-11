6d0427e1244b743ff17b19ca0575bba4211bd064
Edizon Layug <Edizon.L@tech.ducatus.net>
2018-12-12 00:29:06 +0800

    Added event handling

--- src/index.js
+++ src/index.js
@@ -8,28 +8,34 @@ const SelectOption = (props) => {
     )
 }
 
-const FormType = (props)=>{
-    if(props.type == 'dropdown'){
+class FormType extends Component{
+    changeTrigger = (e)=>{
+        this.props.handleInputChange(e)
+    }
+    render(){
+        let stateProp = this.props.name.replace(/_([a-z])/g, (g) => g[1].toUpperCase())
+        if(this.props.type == 'dropdown'){
+            return (
+                <div>
+                    <select name={stateProp} value={this.props[stateProp]} onChange={this.changeTrigger}>
+                        {this.props.options.map((opt,index)=><SelectOption key={index} option={opt}/>)}
+                    </select>
+                </div>
+            )
+        }
         return (
             <div>
-                <select>
-                    {props.options.map((opt,index)=><SelectOption key={index} option={opt}/>)}
-                </select>
+                <input name={stateProp} onChange={this.changeTrigger} placeholder={this.props.label} type={this.props.type} />
             </div>
         )
     }
-    return (
-        <div>
-            <input placeholder={props.label} type={props.type} />
-        </div>
-    )
 }
 
 const QuestionFormField = (props) =>{
     return (
         <div>
-            <lable>{props.label}:</lable>
-            <FormType {...props}/>
+            <label>{props.label}:</label>
+            <FormType {...props.inputField}  handleInputChange={props.handleInputChange} {...props}/>
         </div>
     )
 }
@@ -39,7 +45,7 @@ const Subquestions = (props) =>{
     return (
         <div>
             <legend>{title}</legend>
-            {fields.map((field,index)=><QuestionFormField key={index} {...field} />)}
+            {fields.map((field,index)=><QuestionFormField inputField={props.inputField} handleInputChange={props.handleInputChange} key={index} {...field} />)}
         </div>
     )
 }
@@ -48,39 +54,42 @@ const QuestionsList = (props) =>{
     const {questions} = props
     return (
         <div>
-            {questions.map((question,index)=> <Subquestions key={index} {...question}/>)}
+            {questions.map((question,index)=> <Subquestions handleInputChange={props.handleInputChange} inputField={props.fields} key={index} {...question}/>)}
         </div>
     )
 }
 
 class App extends Component{
-    state = {
-        "questions": [
-          {
-            "title": "Tell us about yourself",
-            "fields": [
-              { "name": "first_name", "label": "First Name", "type": "text" },
-              { "name": "last_name", "label": "Last Name", "type": "text" },
-              { "name": "email", "label": "Email", "type": "text" },
-              { "name": "phone_number", "label": "Phone Number", "type": "text" }
-            ]
-          },
-          {
-            "title": "Where do you live?",
-            "fields": [
-              { "name": "street_address", "label": "Street Address", "type": "text" },
-              { "name": "post_code", "label": "Post Code", "type": "text" },
-              { "name": "country", "label": "Country", "type": "dropdown", "options": ["Canada", "USA"] },
-            ]
-          }
-        ]
+    state = {}
+    handleSubmit = (e) => {
+        e.preventDefault() 
+        for(let key of Object.keys(this.state.fields)){
+            console.log(key,':',this.state.fields[key])
+        }
+    }
+    handleInputChange = (e) =>{
+        let property = e.target.getAttribute("name")
+        let value = e.target.value
+        this.setState(prevState=>({fields:Object.assign(prevState.fields,{[property]:value})}))
+    }
+    componentWillMount() {
+        const fields = {}
+        this.props.config.questions.map((question)=>{
+            question.fields.map((field)=>{
+                fields[field.name.replace(/_([a-z])/g, (g) => g[1].toUpperCase())] = ''
+            })
+        })
+        this.setState({
+            fields
+        })
     }
     render(){
         return (
             <div>
                 <h1>No Issue</h1>
-                <form onSubmit={()=>this.handleSubmit}>
-                    <QuestionsList questions={this.state.questions}/>
+                <form onSubmit={this.handleSubmit}>
+                    <QuestionsList handleInputChange={this.handleInputChange} {...this.state} questions={this.props.config.questions}/>
+                    <button type="submit">Submit</button>
                 </form>
             </div>
 
@@ -88,7 +97,29 @@ class App extends Component{
     }
 }
 
+const formConfig = {
+    "questions": [
+    {
+        "title": "Tell us about yourself",
+        "fields": [
+        { "name": "first_name", "label": "First Name", "type": "text" },
+        { "name": "last_name", "label": "Last Name", "type": "text" },
+        { "name": "email", "label": "Email", "type": "text" },
+        { "name": "phone_number", "label": "Phone Number", "type": "text" }
+        ]
+    },
+    {
+        "title": "Where do you live?",
+        "fields": [
+        { "name": "street_address", "label": "Street Address", "type": "text" },
+        { "name": "post_code", "label": "Post Code", "type": "text" },
+        { "name": "country", "label": "Country", "type": "dropdown", "options": ["Canada", "USA"] },
+        ]
+    }
+    ]
+}
+
 ReactDOM.render(
-  <App/>,
+  <App config={formConfig} />,
   document.getElementById("root")
 );
